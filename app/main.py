73adf11575fd0676f7370c70edffc9beb8ea6c62
elams18 <elamathis18@gmail.com>
2024-05-26 21:49:11 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,14 +5,32 @@ from datetime import datetime, timedelta
 import time
 import argparse
 
+redis_data = {}
+server_data = {}
+BUFFER_SIZE = 4096
+
 def encode_bulk_string(s: str) -> bytes:
     return ("$"+ str(len(s)) + "\r\n" + s + "\r\n").encode()
 
 def encode_error_message(err: str) -> bytes:
     return ("-ERR " + err+ "\r\n").encode()
+    
+def expiration_cleanup(redis_data: dict):
+    while True:
+        # Iterate over keys and remove expired keys
+        for key in list(redis_data.keys()):
+            if redis_data[key].get('expiry') and redis_data[key]['expiry'] < datetime.now():
+                del redis_data[key]
+        # Sleep for some time before checking again (e.g., every minute)
+        time.sleep(60)
 
+def send_server_info(client, replica):
+    result = "role:"+("master" if not replica else "slave") + "\r\n"
+    result = result + "master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb" + "\r\n"
+    result = result + "master_repl_offset:0" + "\r\n"
+    client.send(encode_bulk_string(result)) 
 
-def handle_client(client, redis_data: dict, is_master=False):
+def handle_client(client, redis_data: dict, replica=None):
     def split_segments(s):
         segments = s.split('$')
         if segments[0].startswith('*'):
@@ -24,16 +42,10 @@ def handle_client(client, redis_data: dict, is_master=False):
             processed_segment = re.sub(r'^\d+\r\n', '', seg)
             processed_segment = processed_segment.rstrip('\r\n')
             processed_segments.append(processed_segment)
-        return processed_segments
-    
-    def send_server_info(client):
-        result = "role:"+("master" if is_master else "slave") + "\r\n"
-        result = result + "master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb" + "\r\n"
-        result = result + "master_repl_offset:0" + "\r\n"
-        client.send(encode_bulk_string(result))
-    
+        return processed_segments   
+
     while client:
-        req = client.recv(4096)
+        req = client.recv(BUFFER_SIZE)
         data: str = req.decode() 
         cmds_list = split_segments(data)
         cmds = iter(cmds_list)
@@ -101,44 +113,51 @@ def handle_client(client, redis_data: dict, is_master=False):
             if cmd.lower() == 'info':
                 try:
                     repl = next(cmds)
-                    send_server_info(client)
+                    send_server_info(client, replica)
                 except StopIteration:
                     break
 
-def main(port=6379, is_master=False):
+def connect_to_master(host, port):
+    with socket.create_connection(("localhost", port)) as s:
+        s.send(b"*1\r\n$4\r\nPING\r\n")
+        res = s.recv(BUFFER_SIZE)
 
-    def expiration_cleanup(redis_data: dict):
-        while True:
-            # Iterate over keys and remove expired keys
-            for key in list(redis_data.keys()):
-                if redis_data[key].get('expiry') and redis_data[key]['expiry'] < datetime.now():
-                    del redis_data[key]
-            # Sleep for some time before checking again (e.g., every minute)
-            time.sleep(60)
-
-    print(f"Logs from your program will appear here in port {port}!")
-    server_socket = socket.create_server(("localhost", port), reuse_port=True)
-    redis_data = {}
-
+def main(port=6379, replica=None, from_replica=False):
     # Start expiration cleanup thread
     cleanup_thread = threading.Thread(target=expiration_cleanup, args=(redis_data,))
     cleanup_thread.daemon = True
     cleanup_thread.start()
+        
 
+    print(f"Logs from your program will appear here in port {port}!")
+    server_socket = socket.create_server(("localhost", port), reuse_port=True)
+ 
     while True:
-        client, address = server_socket.accept() # wait for client
-        thread = threading.Thread(target=handle_client, args=(client, redis_data, is_master))
+        client, _ = server_socket.accept() # wait for client
+        server_data[port] = server_socket
+        thread = threading.Thread(target=handle_client, args=(client, redis_data, replica))
         thread.start()
 
 if __name__ == "__main__":
-    argsParser = argparse.ArgumentParser("A Redis server written in Python")
-    argsParser.add_argument("--port", type=int, dest="port", default=6379)
-    argsParser.add_argument("--replicaof", type=str, dest="replica")
-    args = argsParser.parse_args()
-    port = args.port
-    
-    replica = args.replica
-    if replica:
-        main(port, False)
-    else:
-        main(port, True)
+    try:
+        argsParser = argparse.ArgumentParser("A Redis server written in Python")
+        argsParser.add_argument("--port", dest="port", default=6379)
+        argsParser.add_argument("--replicaof", type=str, dest="replica")
+        args = argsParser.parse_args()
+        port = int(args.port)
+        
+        replica = args.replica
+        if replica:
+            master_host, master_port = replica.split(" ")[0], replica.split(" ")[1]
+            master_port = int(master_port)
+            master_server = connect_to_master(master_host, master_port)
+            server_data["master"] = master_server
+            main(port, replica)
+        else:
+            main(port, None)
+    finally:
+        print("CLOSE ALL CONNECTIONS")
+        for repl in server_data:
+            server_data[repl].close()
+
+
